2fe5e28d117c1dbcdfdf0bb69ddd5c136366c8e2
hoang11102002 <thanhhoang11102002@gmail.com>
2025-05-26 01:08:07 +0700

    feat: test

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import { AppProvider } from './provider';
 import { AppRouter } from './router';
 export const App = () => {
+  localStorage.removeItem('accessToken')
+  localStorage.removeItem('currentUser')
   return (
     <AppProvider>
       <AppRouter/>
